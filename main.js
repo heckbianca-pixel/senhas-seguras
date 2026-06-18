224aada2561fa0094361db68e1423ed7b171e592
heckbianca-pixel <heck.bianca@escola.pr.gov.br>
2026-06-18 13:25:38 -0300

    ii

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-document.querySelector('')
+const numeroSenha = document.querySelector('parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+console.log(botoes);
