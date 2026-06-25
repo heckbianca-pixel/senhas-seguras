c2d56faae2ce376bb3cf4728347c55aef6ac37fd
heckbianca-pixel <heck.bianca@escola.pr.gov.br>
2026-06-25 08:37:13 -0300

    ii

--- main.js
+++ main.js
@@ -8,11 +8,22 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
+    if (tamanhoSenha > 1){
+        //tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
     numeroSenha.textContent = tamanhoSenha;
 }
-function aumentaTamanho(){
-    tamanhoSenha = tamanhoSenha+1;
+function aumentaTamanho(){ 
+    if (tamanhoSenha < 20){
+    //tamanhoSenha = tamanhoSenha+1;
+    tamanhoSenha++;
+    }
     numeroSenha.textContent = tamanhoSenha;
 }
-console.log(botoes);
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+campoSenha.value = letrasMaiusculas;
