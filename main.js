109dd6f39ce25cd97ddb6006ee5da57197314af0
heckbianca-pixel <heck.bianca@escola.pr.gov.br>
2026-07-09 13:04:55 -0300

    jj

--- main.js
+++ main.js
@@ -20,10 +20,22 @@ function aumentaTamanho(){
     tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 const campoSenha = document.querySelector('#campo-senha');
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+        console.log(numeroAleatorio);
+    }
+}
 
 campoSenha.value = letrasMaiusculas;
