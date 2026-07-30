fa0f40ad470f8befbcef0cd08f127ebd7ada1ecd
heckbianca-pixel <heck.bianca@escola.pr.gov.br>
2026-07-30 08:50:30 -0300

    dddd

--- main.js
+++ main.js
@@ -41,6 +41,13 @@ function geraSenha(){
     if (checkbox[0].checked){
         alfabeto = alfabeto + letrasMaiusculas;
     }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2]. checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked)
     console.log(alfabeto);
     let senha = '';
     for (let i = 0; i < tamanhoSenha;i++){
